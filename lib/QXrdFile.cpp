5f498a026334b65fe54f7b99e186368fb7bc35ad
Jens Wienkenhoever <wienkenhoever@physik.rwth-aachen.de>
2013-10-14 14:55:44 +0200

    Add pragma marks for better overview in XCode

--- lib/QXrdFile.cpp
+++ lib/QXrdFile.cpp
@@ -1,5 +1,12 @@
 #include "QXrdFile_p.h"
 
+//////////////// QXrdFile ////////////////
+#ifdef Q_WS_MAC
+#pragma mark QXrdFile
+#pragma mark -
+#pragma mark Constructors
+#endif
+
 QXrdFile::QXrdFile() :
   QIODevice(), d_ptr(new QXrdFilePrivate(this)) {
 }
@@ -16,6 +23,9 @@ QXrdFile::QXrdFile(QObject *parent, const QString &path) :
   QIODevice(parent), d_ptr(new QXrdFilePrivate(this)) {
 }
 
+#ifdef Q_WS_MAC
+#pragma mark Protected constructors
+#endif
 QXrdFile::QXrdFile(QXrdFilePrivate &d) :
   d_ptr(&d) {
 }
@@ -31,5 +41,12 @@ QXrdFile::QXrdFile(QObject *parent, QXrdFilePrivate &d) :
 QXrdFile::QXrdFile(QObject *parent, const QString &path, QXrdFilePrivate &d) :
   QIODevice(parent), d_ptr(&d) {
 }
+
+//////////////// QXrdFilePrivate ////////////////
+#ifdef Q_WS_MAC
+#pragma mark - QXrdFilePrivate
+#pragma mark -
+#pragma mark Constructors
+#endif
 
 QXrdFilePrivate::QXrdFilePrivate(QXrdFile* q) : q_ptr(q) { }
